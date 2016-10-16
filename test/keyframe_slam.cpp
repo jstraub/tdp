7b8e3cece4e251688e2b7b524c72912fb38c4796
Julian Straub <jstraub@csail.mit.edu>
2016-10-16 17:30:36 -0400

    raw GTSAM test donw and not working either

--- test/keyframe_slam.cpp
+++ test/keyframe_slam.cpp
@@ -5,33 +5,35 @@
 
 using namespace tdp;
 
-TEST(setup, KeyframeSLAM) {
-  ManagedHostImage<Vector3fda> dummyPc(1,1);
-  ManagedHostImage<Vector3fda> dummyN(1,1);
-  ManagedHostImage<Vector3bda> dummyRgb(1,1);
-  SE3f T_wk0;
-  SE3f T_wk1 (SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1));
-  SE3f dT_01 (SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1.1));
-
-  KeyframeSLAM kfSLAM;  
-  kfSLAM.AddKeyframe(dummyPc, dummyN, dummyRgb, T_wk0);
-  kfSLAM.AddKeyframe(dummyPc, dummyN, dummyRgb, T_wk1);
-//  kfSLAM.AddLoopClosure(0,1,dT_01);
-
-  kfSLAM.Optimize();
-}
+//TEST(setup, KeyframeSLAM) {
+//  ManagedHostImage<Vector3fda> dummyPc(1,1);
+//  ManagedHostImage<Vector3fda> dummyN(1,1);
+//  ManagedHostImage<Vector3bda> dummyRgb(1,1);
+//  SE3f T_wk0;
+//  SE3f T_wk1 (SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1));
+//  SE3f dT_01 (SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1.1));
+//
+//  KeyframeSLAM kfSLAM;  
+//  kfSLAM.AddKeyframe(dummyPc, dummyN, dummyRgb, T_wk0);
+//  kfSLAM.AddKeyframe(dummyPc, dummyN, dummyRgb, T_wk1);
+////  kfSLAM.AddLoopClosure(0,1,dT_01);
+//
+//  kfSLAM.Optimize();
+//}
 
 TEST(setupRaw, KeyframeSLAM) {
-  model = gtsam::noiseModel::Diagonal::Variances(
-        (gtsam::Vector(6) << 1e-6, 1e-6, 1e-6, 1e-4, 1e-4, 1e-4).finished());
+  gtsam::SharedNoiseModel model = gtsam::noiseModel::Diagonal::Variances(
+        (gtsam::Vector(6) << 1e-2, 1e-2, 1e-2, 1e-2, 1e-2, 1e-2).finished());
 
   gtsam::Values::shared_ptr initials(new gtsam::Values);
   gtsam::NonlinearFactorGraph::shared_ptr graph(new gtsam::NonlinearFactorGraph);
 
+  // set the poses up using SE3 class 
   SE3f T_wk0;
-  SE3f T_wk1 (SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1));
-  SE3f dT_01 (SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1.1));
+  SE3f T_wk1(SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1));
+  SE3f dT_01(SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1.1));
 
+  // setup Pose3s for GTSAM
   gtsam::Pose3 poseA = gtsam::Pose3(T_wk0.matrix().cast<double>());
   gtsam::Pose3 poseB = gtsam::Pose3(T_wk1.matrix().cast<double>());
   gtsam::Pose3 poseAB = poseA.between(poseB);
@@ -40,19 +42,22 @@ TEST(setupRaw, KeyframeSLAM) {
   initials->insert(0, poseA);
   initials->insert(1, poseB);
 
-  graph->add(gtsam::PriorFactor<gtsam::Pose3>(0, poseA, model));
-  gtsam::NonlinearFactor::shared_ptr factor(
+  gtsam::PriorFactor<gtsam::Pose3>::shared_ptr factor0(
+      new gtsam::PriorFactor<gtsam::Pose3>(0, poseA, model));
+  graph->add(factor0);
+  gtsam::NonlinearFactor::shared_ptr factor01(
       new gtsam::BetweenFactor<gtsam::Pose3>(0, 1, poseAB, model));
-  gtsam::NonlinearFactor::shared_ptr factor(
+  graph->push_back(factor01);
+  gtsam::NonlinearFactor::shared_ptr factor01_obs(
       new gtsam::BetweenFactor<gtsam::Pose3>(0, 1, poseAB_obs, model));
-  graph->push_back(factor);
+  graph->push_back(factor01_obs);
 
   graph->print();
 
   gtsam::GaussNewtonParams params;
   params.setVerbosity("ERROR"); 
   params.setMaxIterations(1);
-  gtsam::GaussNewtonOptimizer optimizer(*graph_, *initials_, params);
+  gtsam::GaussNewtonOptimizer optimizer(*graph, *initials, params);
   gtsam::Values results = optimizer.optimize();
 }
 
