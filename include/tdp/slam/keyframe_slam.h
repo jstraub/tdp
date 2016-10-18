c6e75c7c9356e69765a286afb6501a686116bac2
Julian Straub <jstraub@csail.mit.edu>
2016-10-18 11:29:45 -0400

    ISAM test seems to work

--- include/tdp/slam/keyframe_slam.h
+++ include/tdp/slam/keyframe_slam.h
@@ -1,32 +1,28 @@
 #pragma once
 #include <tdp/slam/keyframe.h>
 
-#include <gtsam/slam/dataset.h>
-#include <gtsam/slam/BetweenFactor.h>
-#include <gtsam/nonlinear/NonlinearEquality.h>
-#include <gtsam/slam/PriorFactor.h>
-#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
-#include <gtsam/nonlinear/DoglegOptimizer.h>
-#include <gtsam/nonlinear/ISAM2.h>
+#include <isam/isam.h>
+#include <isam/Slam.h>
+#include <isam/robust.h>
 
 namespace tdp {
 
 class KeyframeSLAM {
  public:
-  KeyframeSLAM() 
-    : initials_(new gtsam::Values), graph_(new gtsam::NonlinearFactorGraph)
-  {
-    priorModel_ = 
-      gtsam::noiseModel::Diagonal::Variances(
-          (gtsam::Vector(6) << 1e-6, 1e-6, 1e-6, 1e-4, 1e-4, 1e-4).finished());
-    odomModel_ = 
-      gtsam::noiseModel::Diagonal::Variances(
-          (gtsam::Vector(6) << 1e-4, 1e-4, 1e-4, 1e-2, 1e-2, 1e-2).finished());
-    loopModel_ = 
-      gtsam::noiseModel::Diagonal::Variances(
-          (gtsam::Vector(6) << 1e-4, 1e-4, 1e-4, 1e-2, 1e-2, 1e-2).finished());
-
+  KeyframeSLAM() :
+    noisePrior_(isam::Information(10000. * isam::eye(6))),
+    noiseOdom_(isam::Information(1000. * isam::eye(6))),
+    noiseLoopClosure_(isam::Information(1000. * isam::eye(6)))
+  { 
+    isam::Properties props;
+    props.verbose=true;
+    props.method=isam::Method::GAUSS_NEWTON;
+    props.method=isam::Method::LEVENBERG_MARQUARDT;
+    props.epsilon_rel = 1e-6;
+    props.epsilon_abs = 1e-6;
+    slam_.set_properties(props); 
   };
+
   ~KeyframeSLAM()
   {};
 
@@ -36,153 +32,186 @@ class KeyframeSLAM {
       const SE3f& T_wk) {
     kfs_.emplace_back(pc, n, rgb, T_wk);
     if (kfs_.size() == 1) {
-      gtsam::Pose3 poseA = gtsam::Pose3(T_wk.matrix().cast<double>());
-      initials_->insert(0, poseA);
-//      graph_->add(gtsam::NonlinearEquality<gtsam::Pose3>(0, poseA));
-      graph_->add(gtsam::PriorFactor<gtsam::Pose3>(0, poseA, priorModel_));
+      isam::Pose3d origin = isam::Pose3d(T_wk.matrix().cast<double>());
+
+      isam::Pose3d_Node* pose0 = new isam::Pose3d_Node();
+      slam_.add_node(pose0);
+      T_wk_.push_back(pose0);
+
+      isam::Pose3d_Factor* prior = new isam::Pose3d_Factor(pose0,
+          origin, noisePrior_);
+      slam_.add_factor(prior);
     } else {
       int idA = kfs_.size() - 2;
       int idB = kfs_.size() - 1;
       tdp::SE3f T_ab = kfs_[idA].T_wk_.Inverse() * kfs_[idB].T_wk_;
-      // poseA.between(poseB) = T_ab
-      gtsam::Pose3 poseAB = gtsam::Pose3(T_ab.matrix().cast<double>());
-      gtsam::Pose3 poseA = gtsam::Pose3(kfs_[idA].T_wk_.matrix().cast<double>());
-      gtsam::Pose3 poseB = gtsam::Pose3(kfs_[idB].T_wk_.matrix().cast<double>());
 
-//      poseA.print("A");
-//      poseB.print("B");
-//      poseAB.print("AB SE3f");
-//      poseA.between(poseB).print("AB ");
+      isam::Pose3d poseAB(T_ab.matrix().cast<double>());
 
-      initials_->insert(idB, poseB);
-      gtsam::NonlinearFactor::shared_ptr factor(
-          new gtsam::BetweenFactor<gtsam::Pose3>(idA, idB, poseAB, odomModel_));
-      graph_->push_back(factor);
+      isam::Pose3d_Node* poseNext = new isam::Pose3d_Node();
+      slam_.add_node(poseNext);
+      T_wk_.push_back(poseNext);
+
+      isam::Pose3d_Pose3d_Factor* odo = new isam::Pose3d_Pose3d_Factor(
+          T_wk_[idA], T_wk_[idB], poseAB, noiseOdom_);
+      slam_.add_factor(odo);
     }
   }
 
   void AddLoopClosure(int idA, int idB, const SE3f& T_ab) {
-    gtsam::NonlinearFactor::shared_ptr factor(
-      new gtsam::BetweenFactor<gtsam::Pose3>(idA, idB, 
-        gtsam::Pose3(T_ab.matrix().cast<double>()), loopModel_));
-    graph_->push_back(factor);
+
+    isam::Pose3d poseAB(T_ab.matrix().cast<double>());
+
+    isam::Pose3d_Pose3d_Factor* loop = new isam::Pose3d_Pose3d_Factor(
+        T_wk_[idA], T_wk_[idB], poseAB, noiseLoopClosure_);
+    slam_.add_factor(loop);
   }
 
-//  void PrintGraph() {
-//    for(boost::shared_ptr<gtsam::NonlinearFactor> factor_: *graph_) {
-//      boost::shared_ptr< gtsam::BetweenFactor<gtsam::Pose3> > factor3D =
-//        boost::dynamic_pointer_cast< gtsam::BetweenFactor<gtsam::Pose3> >(factor_);
-//      if (factor3D){
-//        gtsam::SharedNoiseModel model = factor3D->noiseModel();
-//
-//        boost::shared_ptr<gtsam::noiseModel::Gaussian> gaussianModel =
-//          boost::dynamic_pointer_cast<gtsam::noiseModel::Gaussian>(model);
-////        if (!gaussianModel){
-////          model->print("model\n");
-////          std::cerr << "writeG2o: invalid noise model!" << std::endl;
-////          continue;
-////        }
-////        gtsam::Matrix Info = gaussianModel->R().transpose() * gaussianModel->R();
-//        gtsam::Pose3 pose3D = factor3D->measured();
-//        gtsam::Point3 p = pose3D.translation();
-//        gtsam::Rot3 R = pose3D.rotation();
-//
-//        std::cout << "EDGE_SE3:QUAT " << factor3D->key1() 
-//          << " " << factor3D->key2() << " "
-//          << p.x() << " "  << p.y() << " " << p.z()  
-//          << " " << R.toQuaternion().x() << " " << R.toQuaternion().y() << " " 
-//          << R.toQuaternion().z()  << " " << R.toQuaternion().w() 
-//          << std::endl;
-//
-//        factor3D->print(" -- ");
-//
-////        gtsam::Matrix InfoG2o = gtsam::I_6x6;
-////        InfoG2o.block(0,0,3,3) = Info.block(3,3,3,3); // cov translation
-////        InfoG2o.block(3,3,3,3) = Info.block(0,0,3,3); // cov rotation
-////        InfoG2o.block(0,3,3,3) = Info.block(0,3,3,3); // off diagonal
-////        InfoG2o.block(3,0,3,3) = Info.block(3,0,3,3); // off diagonal
-////
-////        for (int i = 0; i < 6; i++){
-////          for (int j = i; j < 6; j++){
-////            std::cout  << " " << InfoG2o(i, j);
-////          }
-////        }
-////        std::cout << std::endl;
-//      }
-//
-//      boost::shared_ptr< gtsam::PriorFactor<gtsam::Pose3> > factorPrior3D =
-//        boost::dynamic_pointer_cast< gtsam::PriorFactor<gtsam::Pose3> >(factor_);
-//      if (factorPrior3D) {
-//        factorPrior3D->print("-- ");
-//      }
-//    }
-//  }
-
-  void PrintValues(gtsam::Values& values) {
-
-    gtsam::Values::ConstFiltered<gtsam::Pose3> viewPose3 
-      = values.filter<gtsam::Pose3>();
-    for(const gtsam::Values::ConstFiltered<gtsam::Pose3>::KeyValuePair& 
-        key_value: viewPose3) {
-      gtsam::Point3 p = key_value.value.translation();
-      gtsam::Rot3 R = key_value.value.rotation();
-      std::cout << "VERTEX_SE3:QUAT " << key_value.key 
-        << " " << p.x() << " "  << p.y() << " " << p.z()
-        << " " << R.toQuaternion().x() << " " << R.toQuaternion().y() 
-        << " " << R.toQuaternion().z()
-        << " " << R.toQuaternion().w() << std::endl;
+  void PrintValues() {
+    for (auto& T_wk : T_wk_) {
+      std::cout << T_wk->value() << std::endl;
     }
   }
 
   void Optimize() {
-
-//
-    std::cout << "initial error=" <<graph_->error(*initials_)<< std::endl;
-    PrintValues(*initials_);
-
-    graph_->print();
-//    PrintGraph();
+    std::cout << "initial"<< std::endl;
+    PrintValues();
 
     std::cout << "SAM optimization" << std::endl;
-    gtsam::GaussNewtonParams params;
-    params.setVerbosity("ERROR"); 
-    params.setMaxIterations(100);
-    gtsam::GaussNewtonOptimizer optimizer(*graph_, *initials_, params);
+//    slam_.batch_optimization();
+    slam_.update();
+    slam_.print_stats();
+
+    slam_.save("isam.csv");
+    slam_.print_graph();
     
-//    gtsam::DoglegParams params;
-//    params.setVerbosity("VERBOSE");
-//    gtsam::DoglegOptimizer optimizer(*graph_, *initials_, params);
-
-//    std::cout << "one iteration" << std::endl;
-//    gtsam::GaussianFactorGraph::shared_ptr graph1 =  optimizer.iterate();
-//    std::cout << "one iteration done " << std::endl;
-//    std::cout << "after one iteration " << std::endl;
-//    graph1->print() ;
-
-    gtsam::Values results = optimizer.optimize();
-    std::cout << "after one iteration error=" <<graph_->error(results)<< std::endl;
-    PrintValues(results);
-
-//    gtsam::ISAM2Params params;
-//    params.relinearizeSkip = 1;
-//    gtsam::ISAM2 optimizer(params);
-//
-//    std::cout << "ISAM update" << std::endl;
-//    optimizer.update(*graph_, *initials_);
-//    gtsam::Values currEst = optimizer.calculateEstimate();
-//    currEst.print("cur Est");
+    std::cout << "after iterations" << std::endl;
+    PrintValues();
   }
 
  private:
   std::vector<KeyFrame> kfs_;
 
-  gtsam::Values::shared_ptr initials_;
-  gtsam::NonlinearFactorGraph::shared_ptr graph_;
+  std::vector<isam::Pose3d_Node*> T_wk_;
 
-  gtsam::SharedNoiseModel priorModel_;
-  gtsam::SharedNoiseModel odomModel_;
-  gtsam::SharedNoiseModel loopModel_;
-
+  isam::Slam slam_;
+  isam::Noise noisePrior_;
+  isam::Noise noiseOdom_;
+  isam::Noise noiseLoopClosure_;
 };
 
 }
+
+
+//#include <gtsam/slam/dataset.h>
+//#include <gtsam/slam/BetweenFactor.h>
+//#include <gtsam/nonlinear/NonlinearEquality.h>
+//#include <gtsam/slam/PriorFactor.h>
+//#include <gtsam/nonlinear/GaussNewtonOptimizer.h>
+//#include <gtsam/nonlinear/DoglegOptimizer.h>
+//#include <gtsam/nonlinear/ISAM2.h>
+//
+//namespace tdp {
+//
+//class KeyframeSLAM {
+// public:
+//  KeyframeSLAM() 
+//    : initials_(new gtsam::Values), graph_(new gtsam::NonlinearFactorGraph)
+//  {
+//    priorModel_ = 
+//      gtsam::noiseModel::Diagonal::Variances(
+//          (gtsam::Vector(6) << 1e-6, 1e-6, 1e-6, 1e-4, 1e-4, 1e-4).finished());
+//    odomModel_ = 
+//      gtsam::noiseModel::Diagonal::Variances(
+//          (gtsam::Vector(6) << 1e-4, 1e-4, 1e-4, 1e-2, 1e-2, 1e-2).finished());
+//    loopModel_ = 
+//      gtsam::noiseModel::Diagonal::Variances(
+//          (gtsam::Vector(6) << 1e-4, 1e-4, 1e-4, 1e-2, 1e-2, 1e-2).finished());
+//
+//  };
+//  ~KeyframeSLAM()
+//  {};
+//
+//  void AddKeyframe(const Image<Vector3fda>& pc, 
+//      const Image<Vector3fda>& n,
+//      const Image<Vector3bda>& rgb,
+//      const SE3f& T_wk) {
+//    kfs_.emplace_back(pc, n, rgb, T_wk);
+//    if (kfs_.size() == 1) {
+//      gtsam::Pose3 poseA = gtsam::Pose3(T_wk.matrix().cast<double>());
+//      initials_->insert(0, poseA);
+////      graph_->add(gtsam::NonlinearEquality<gtsam::Pose3>(0, poseA));
+//      graph_->add(gtsam::PriorFactor<gtsam::Pose3>(0, poseA, priorModel_));
+//    } else {
+//      int idA = kfs_.size() - 2;
+//      int idB = kfs_.size() - 1;
+//      tdp::SE3f T_ab = kfs_[idA].T_wk_.Inverse() * kfs_[idB].T_wk_;
+//      // poseA.between(poseB) = T_ab
+//      gtsam::Pose3 poseAB = gtsam::Pose3(T_ab.matrix().cast<double>());
+//      gtsam::Pose3 poseA = gtsam::Pose3(kfs_[idA].T_wk_.matrix().cast<double>());
+//      gtsam::Pose3 poseB = gtsam::Pose3(kfs_[idB].T_wk_.matrix().cast<double>());
+//
+//      initials_->insert(idB, poseB);
+//      gtsam::NonlinearFactor::shared_ptr factor(
+//          new gtsam::BetweenFactor<gtsam::Pose3>(idA, idB, poseAB, odomModel_));
+//      graph_->push_back(factor);
+//    }
+//  }
+//
+//  void AddLoopClosure(int idA, int idB, const SE3f& T_ab) {
+//    gtsam::NonlinearFactor::shared_ptr factor(
+//      new gtsam::BetweenFactor<gtsam::Pose3>(idA, idB, 
+//        gtsam::Pose3(T_ab.matrix().cast<double>()), loopModel_));
+//    graph_->push_back(factor);
+//  }
+//
+//  void PrintValues(gtsam::Values& values) {
+//
+//    gtsam::Values::ConstFiltered<gtsam::Pose3> viewPose3 
+//      = values.filter<gtsam::Pose3>();
+//    for(const gtsam::Values::ConstFiltered<gtsam::Pose3>::KeyValuePair& 
+//        key_value: viewPose3) {
+//      gtsam::Point3 p = key_value.value.translation();
+//      gtsam::Rot3 R = key_value.value.rotation();
+//      std::cout << "VERTEX_SE3:QUAT " << key_value.key 
+//        << " " << p.x() << " "  << p.y() << " " << p.z()
+//        << " " << R.toQuaternion().x() << " " << R.toQuaternion().y() 
+//        << " " << R.toQuaternion().z()
+//        << " " << R.toQuaternion().w() << std::endl;
+//    }
+//  }
+//
+//  void Optimize() {
+//
+////
+//    std::cout << "initial error=" <<graph_->error(*initials_)<< std::endl;
+//    PrintValues(*initials_);
+//
+//    graph_->print();
+////    PrintGraph();
+//
+//    std::cout << "SAM optimization" << std::endl;
+//    gtsam::GaussNewtonParams params;
+//    params.setVerbosity("ERROR"); 
+//    params.setMaxIterations(100);
+//    gtsam::GaussNewtonOptimizer optimizer(*graph_, *initials_, params);
+//    
+//    gtsam::Values results = optimizer.optimize();
+//    std::cout << "after one iteration error=" <<graph_->error(results)<< std::endl;
+//    PrintValues(results);
+//
+//  }
+//
+// private:
+//  std::vector<KeyFrame> kfs_;
+//
+//  gtsam::Values::shared_ptr initials_;
+//  gtsam::NonlinearFactorGraph::shared_ptr graph_;
+//
+//  gtsam::SharedNoiseModel priorModel_;
+//  gtsam::SharedNoiseModel odomModel_;
+//  gtsam::SharedNoiseModel loopModel_;
+//
+//};
+//
+//}
--- test/isam.cpp
+++ test/isam.cpp
@@ -1,12 +1,39 @@
 #include <tdp/testing/testing.h>
 #include <Eigen/Dense>
-#include <isam/isam.h>
+#include <tdp/slam/keyframe_slam.h>
+#include <tdp/data/managed_image.h>
 
+#include <isam/isam.h>
 #include <isam/Slam.h>
 #include <isam/robust.h>
 
+using namespace tdp; 
+TEST(setup, KeyframeSLAM) {
+  ManagedHostImage<Vector3fda> dummyPc(1,1);
+  ManagedHostImage<Vector3fda> dummyN(1,1);
+  ManagedHostImage<Vector3bda> dummyRgb(1,1);
+  SE3f T_wk0;
+  SE3f T_wk1 (SO3f::Rx(5.*M_PI/180.), Eigen::Vector3f(0,0,1));
+  SE3f dT_01 (SO3f::Rx(6.*M_PI/180.), Eigen::Vector3f(0,0,1.1));
+
+  KeyframeSLAM kfSLAM;  
+  kfSLAM.AddKeyframe(dummyPc, dummyN, dummyRgb, T_wk0);
+  kfSLAM.AddKeyframe(dummyPc, dummyN, dummyRgb, T_wk1);
+  kfSLAM.AddLoopClosure(0,1,dT_01);
+
+  kfSLAM.Optimize();
+}
+
 TEST(setup, isam) {
   isam::Slam slam;
+
+  isam::Properties props;
+  props.verbose=true;
+  props.method=isam::Method::GAUSS_NEWTON;
+  props.method=isam::Method::LEVENBERG_MARQUARDT;
+  props.epsilon_rel = 1e-6;
+  props.epsilon_abs = 1e-6;
+  slam.set_properties(props); 
 
   float a = 5.*M_PI/180.;
   Eigen::Matrix3d Ra;
