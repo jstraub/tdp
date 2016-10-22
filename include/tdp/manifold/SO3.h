a3c4644bf53e3b494e5be59fc89099b349aed393
Julian Straub <jstraub@csail.mit.edu>
2016-10-22 13:20:38 -0400

    S3 -> SO3

--- include/tdp/manifold/SO3.h
+++ include/tdp/manifold/SO3.h
@@ -1,96 +1,100 @@
 #pragma once
 #include <iostream>
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
 #include <tdp/config.h>
 #include <tdp/manifold/manifold.h>
+#include <tdp/manifold/S.h>
 
 namespace tdp {
 
-template<typename T>
+template<typename T, int Options=Eigen::ColMajor>
 class SO3 : Manifold<T,3> {
  public:
   TDP_HOST_DEVICE
   SO3();
-
   TDP_HOST_DEVICE
   SO3(const Eigen::Matrix<T,3,3>& R);
-
   TDP_HOST_DEVICE
-  SO3(const SO3<T>& other);
-
+  SO3(const SO3<T,Options>& other);
   TDP_HOST_DEVICE
-  SO3(const Eigen::Quaternion<T>& q);
+  SO3(const Eigen::Quaternion<T,Options>& q);
+  TDP_HOST_DEVICE
+  SO3(const Eigen::Matrix<T,3,1>& axis, T angle);
+  TDP_HOST_DEVICE
+  SO3(const Eigen::Matrix<T,3,1>& axisAngle);
 
   TDP_HOST_DEVICE
   ~SO3() {}
 
   TDP_HOST_DEVICE
-  const Eigen::Matrix<T,3,3>& matrix() const { return R_;}
+  Eigen::Matrix<T,3,3,Options> matrix() const { 
+    return q_.matrix();
+  }
   TDP_HOST_DEVICE
-  Eigen::Matrix<T,3,3>& matrix() { return R_;}
-
-  Eigen::Quaternion<T> quat() { return Eigen::Quaternion<T>(R_); }
-
-  TDP_HOST_DEVICE
-  SO3<T> Inverse() const ;
-  TDP_HOST_DEVICE
-  SO3<T> Exp (const Eigen::Matrix<T,3,1>& w)const ;
-  TDP_HOST_DEVICE
-  Eigen::Matrix<T,3,1> Log (const SO3<T>& other)const ;
+  Eigen::Matrix<T,4,1,Options> vector() const { 
+    return q_.coeffs();
+  }
 
   TDP_HOST_DEVICE
-  Eigen::Matrix<T,3,1> vee() const;
-
-  Eigen::Matrix<T,3,1> operator-(const SO3<T>& other);
-  SO3<T>& operator+=(const SO3<T>& other);
-  const SO3<T> operator+(const SO3<T>& other) const;
-
-  SO3<T>& operator*=(const SO3<T>& other);
-  const SO3<T> operator*(const SO3<T>& other) const;
-
-  SO3<T>& operator+=(const Eigen::Matrix<T,3,1>& w);
-  const SO3<T> operator+(const Eigen::Matrix<T,3,1>& w);
-
-  // transform 3D data point
+  Eigen::Matrix<T,3,1,Options> ToAxisAngle() const {
+    Eigen::Matrix<T,3,1,Options> aa;
+    T angle;
+    ToAxisAngle(aa, angle);
+    return aa*angle;
+  }
   TDP_HOST_DEVICE
-  Eigen::Matrix<T,3,1> operator*(const Eigen::Matrix<T,3,1>& x) const;
+  void ToAxisAngle(Eigen::Matrix<T,3,1,Options>& axis, T& angle) const {
+    axis = q_.vec().normalized();
+    angle = 2.*acos(q_.w());
+  }
 
-  static Eigen::Matrix<T,3,3> invVee(const Eigen::Matrix<T,3,1>& w);
-  static Eigen::Matrix<T,3,1> vee(const Eigen::Matrix<T,3,3>& W);
-  static Eigen::Matrix<T,3,3> skew(const Eigen::Matrix<T,3,3>& W);
+  TDP_HOST_DEVICE
+  SO3<T,Options> Inverse() const ;
+  TDP_HOST_DEVICE
+  SO3<T,Options> Exp (const Eigen::Matrix<T,3,1>& w) const ;
+  TDP_HOST_DEVICE
+  Eigen::Matrix<T,3,1> Log (const SO3<T,Options>& other) const;
 
-  /// Generator matrices of SO3
-  static Eigen::Matrix<T,3,3> G1();
-  static Eigen::Matrix<T,3,3> G2();
-  static Eigen::Matrix<T,3,3> G3();
-  static Eigen::Matrix<T,3,3> G(uint32_t i);
+  TDP_HOST_DEVICE
+  SO3<T,Options>& operator*=(const SO3<T,Options>& other);
+  TDP_HOST_DEVICE
+  const SO3<T,Options> operator*(const SO3<T,Options>& other) const;
 
-  static Eigen::Matrix<T,3,3> Exp_(const Eigen::Matrix<T,3,1>& w);
-  static Eigen::Matrix<T,3,1> Log_(const Eigen::Matrix<T,3,3>& R);
+  /// transform 3D data point
+  TDP_HOST_DEVICE
+  Eigen::Matrix<T,3,1> operator*(
+      const Eigen::Matrix<T,3,1>& x) const;
 
-  static SO3<T> Random();
+  static SO3<T,Options> Exp_(const Eigen::Matrix<T,3,1>& w);
+  static Eigen::Matrix<T,3,1> Log_(const SO3<T,Options>& R);
+
+  static SO3<T,Options> Random();
 
   /// Elementary rotation matrices about the x,y,z axis by angle alpha
   /// [rad]
-  static SO3<T> Rx(T alpha);
-  static SO3<T> Ry(T alpha);
-  static SO3<T> Rz(T alpha);
+  static SO3<T,Options> Rx(T alpha);
+  static SO3<T,Options> Ry(T alpha);
+  static SO3<T,Options> Rz(T alpha);
 
   /// Rotation matrix from roll pitch yaw (rpy) angles;
-  static SO3<T> R_rpy(Eigen::Matrix<T,3,1> rpy) {
-    return Rz(rpy(0))+Ry(rpy(1))+Rz(rpy(2)); 
+  static SO3<T,Options> R_rpy(Eigen::Matrix<T,3,1> rpy) {
+    return Rz(rpy(0))*Ry(rpy(1))*Rz(rpy(2)); 
   }
 
  private:
-  Eigen::Matrix<T,3,3> R_;
+  Eigen::Quaternion<T,Options> q_;
 };
 
 typedef SO3<double> SO3d;
 typedef SO3<float> SO3f;
+typedef SO3<double,Eigen::DontAlign> SO3dda;
+typedef SO3<float,Eigen::DontAlign> SO3fda;
+template <typename T> using SO3da = SO3<T,Eigen::DontAlign>;
 
-template<typename T>
-std::ostream& operator<<(std::ostream& out, const SO3<T>& so3) {
-  out << so3.matrix();
+template<typename T, int Options>
+std::ostream& operator<<(std::ostream& out, const SO3<T,Options>& s3) {
+  out << s3.vector().transpose() << std::endl;
   return out;
 }
 
@@ -98,4 +102,5 @@ std::ostream& operator<<(std::ostream& out, const SO3<T>& so3) {
 
 }
 #include <tdp/manifold/SO3_impl.hpp>
+
 
--- include/tdp/manifold/SO3_impl.hpp
+++ include/tdp/manifold/SO3_impl.hpp
@@ -2,215 +2,98 @@
 
 namespace tdp {
 
-template<typename T>
-SO3<T>::SO3() 
-  : R_(Eigen::Matrix<T,3,3>::Identity())
+template<typename T, int Options>
+SO3<T,Options>::SO3() 
+  : q_(Eigen::Quaternion<T,Options>::Identity())
 {}
 
-template<typename T>
-SO3<T>::SO3(const Eigen::Matrix<T,3,3>& R) 
-  : R_(R)
+template<typename T, int Options>
+SO3<T,Options>::SO3(const Eigen::Matrix<T,3,3>& R) 
+  : q_(R)
 {}
 
-template<typename T>
-SO3<T>::SO3(const SO3<T>& other)
-  : R_(other.R_)
+template<typename T, int Options>
+SO3<T,Options>::SO3(const SO3<T,Options>& other)
+  : q_(other.q_.normalized())
 {}
 
-template<typename T>
-SO3<T>::SO3(const Eigen::Quaternion<T>& q)
-  : R_(q.toRotationMatrix())
+template<typename T, int Options>
+SO3<T,Options>::SO3(const Eigen::Quaternion<T,Options>& q)
+  : q_(q.normalized())
 {}
 
-template<typename T>
-Eigen::Matrix<T,3,1> SO3<T>::vee() const {
-  return vee(R_);
+template<typename T, int Options>
+SO3<T,Options>::SO3(const Eigen::Matrix<T,3,1>& axis, T angle) 
+  : q_(cos(angle*0.5), 
+      sin(angle*0.5)*axis(0)/axis.norm(),
+      sin(angle*0.5)*axis(1)/axis.norm(),
+      sin(angle*0.5)*axis(2)/axis.norm())
+{ }
+
+template<typename T, int Options>
+SO3<T,Options>::SO3(const Eigen::Matrix<T,3,1>& axisAngle) 
+  : q_(axisAngle.normalized(), axisAngle.norm())
+{ }
+
+template<typename T, int Options>
+SO3<T,Options> SO3<T,Options>::Inverse() const {
+  return SO3<T,Options>(q_.inverse());
 }
 
-template<typename T>
-Eigen::Matrix<T,3,1> SO3<T>::vee(const Eigen::Matrix<T,3,3>& W) {
-  const Eigen::Matrix<T,3,3> A = 0.5*(W - W.transpose());
-  return Eigen::Matrix<T,3,1>(A(2,1), A(0,2), A(1,0));
+template<typename T, int Options>
+SO3<T,Options> SO3<T,Options>::Exp(const Eigen::Matrix<T,3,1>& w) const {
+  return SO3<T,Options>(q_*Exp_(w));
 }
 
-template<typename T>
-Eigen::Matrix<T,3,3> SO3<T>::invVee(const Eigen::Matrix<T,3,1>& w) {
-  Eigen::Matrix<T,3,3> W = Eigen::Matrix<T,3,3>::Zero();
-  W(2,1) = w(0);
-  W(0,2) = w(1);
-  W(1,0) = w(2);
-
-  W(1,2) = -w(0);
-  W(2,0) = -w(1);
-  W(0,1) = -w(2);
-  return W;
-};
-
-template<typename T>
-Eigen::Matrix<T,3,3> SO3<T>::skew(const Eigen::Matrix<T,3,3>& W) {
-  return 0.5*(W-W.transpose());
+template<typename T, int Options>
+Eigen::Matrix<T,3,1> SO3<T,Options>::Log(const SO3<T,Options>& other) const {
+  return Log_(q_.inverse()*other.q_);
 }
 
-template<typename T>
-SO3<T> SO3<T>::Inverse() const {
-  return SO3<T>(R_.transpose());
+template<typename T, int Options>
+SO3<T,Options> SO3<T,Options>::Exp_(const Eigen::Matrix<T,3,1>& w) {
+  return SO3<T,Options>(w);
 }
 
-template<typename T>
-SO3<T> SO3<T>::Exp(const Eigen::Matrix<T,3,1>& w) const {
-  return SO3<T>(R_*Exp_(w));
+template<typename T, int Options>
+Eigen::Matrix<T,3,1> SO3<T,Options>::Log_(const SO3<T,Options>& R) {
+  return R.ToAxisAngle();
 }
 
-template<typename T>
-Eigen::Matrix<T,3,1> SO3<T>::Log(const SO3<T>& other) const {
-  return Log_(R_.transpose()*other.R_);
-}
-
-template<typename T>
-Eigen::Matrix<T,3,3> SO3<T>::Exp_(const Eigen::Matrix<T,3,1>& w) {
-  const T theta = sqrt(w.array().square().matrix().sum());
-  const Eigen::Matrix<T,3,3> W = invVee(w);
-  T a = sin(theta)/theta;
-  if(a!=a) a = 0.0;
-  T b = (1.-cos(theta))/(theta*theta);
-  if(b!=b) b = 0.0;
-  return Eigen::Matrix<T,3,3>::Identity() + a * W + b * W*W;
-}
-
-template<typename T>
-Eigen::Matrix<T,3,1> SO3<T>::Log_(const Eigen::Matrix<T,3,3>& R) {
-  const T theta = acos((R.trace()-1.)*0.5);
-  T a = theta/(2.*sin(theta));
-  if(a!=a) a = 0.0;
-  Eigen::Matrix<T,3,3> W = a*(R-R.transpose());
-  return vee(W);
-}
-
-template<typename T>
-Eigen::Matrix<T,3,1> SO3<T>::operator-(const SO3<T>& other) {
-  return other.Log(*this);
-//  return Log_(other.R_.transpose()*R_);
-}
-
-template<typename T>
-SO3<T>& SO3<T>::operator+=(const SO3<T>& other) {
-  R_ = R_ * other.R_;
+template<typename T, int Options>
+SO3<T,Options>& SO3<T,Options>::operator*=(const SO3<T,Options>& other) {
+  q_ = q_ * other.q_;
+  q_.normalize();
   return *this;
 }
 
-template<typename T>
-const SO3<T> SO3<T>::operator+(const SO3<T>& other) const {
-  return SO3<T>(*this) += other;
+template<typename T, int Options>
+const SO3<T,Options> SO3<T,Options>::operator*(const SO3<T,Options>& other) const {
+  return SO3<T,Options>(*this) *= other;
 }
 
-template<typename T>
-SO3<T>& SO3<T>::operator*=(const SO3<T>& other) {
-  R_ = R_ * other.R_;
-  return *this;
+template<typename T, int Options>
+Eigen::Matrix<T,3,1> SO3<T,Options>::operator*(const Eigen::Matrix<T,3,1>& x) const {
+  return this->q_._transformVector(x);
 }
 
-template<typename T>
-const SO3<T> SO3<T>::operator*(const SO3<T>& other) const {
-  return SO3<T>(*this) *= other;
+template<typename T, int Options>
+SO3<T,Options> SO3<T,Options>::Random() {
+  Eigen::Matrix<T,3,1> w = S<T,3>::Random().vector();
+  return SO3<T,Options>(Exp_(w));
 }
 
-template<typename T>
-SO3<T>& SO3<T>::operator+=(const Eigen::Matrix<T,3,1>& w) {
-  *this = Exp(w);
-  return *this;
+template<typename T, int Options>
+SO3<T,Options> SO3<T,Options>::Rx(T alpha) { 
+  return SO3<T,Options>(Eigen::Matrix<T,3,1,Options>(1.,0.,0.), alpha);
 }
-
-template<typename T>
-const SO3<T> SO3<T>::operator+(const Eigen::Matrix<T,3,1>& w) {
-  return SO3<T>(*this) += w;
+template<typename T, int Options>
+SO3<T,Options> SO3<T,Options>::Ry(T alpha) {
+  return SO3<T,Options>(Eigen::Matrix<T,3,1,Options>(0.,1.,0.), alpha);
 }
-
-template<typename T>
-Eigen::Matrix<T,3,1> SO3<T>::operator*(const Eigen::Matrix<T,3,1>& x) const {
-  return this->R_*x;
-}
-
-//template<typename T>
-//SO3<T> operator+(const SO3<T>& lhs, const SO3<T>& rhs) {
-//  SO3<T> res(lhs);
-//  res += rhs;
-//  return res;
-//}
-
-
-template<typename T>
-Eigen::Matrix<T,3,3> SO3<T>::G(uint32_t i) {
-  if (i==0)
-    return G1();
-  else if (i==1)
-    return G2();
-  else if (i==2)
-    return G3();
-  std::cout << "not a valid Generator id " << i << std::endl;
-  return Eigen::Matrix<T,3,3>::Zero();
-}
-
-template<typename T>
-Eigen::Matrix<T,3,3> SO3<T>::G1() {
-  static Eigen::Matrix<T,3,3> G1_ = Eigen::Matrix<T,3,3>::Zero();
-  G1_(2,1) = 1.;
-  G1_(1,2) = -1.;
-  return G1_;
-}
-
-template<typename T>
-Eigen::Matrix<T,3,3> SO3<T>::G2() {
-  static Eigen::Matrix<T,3,3> G2_ = Eigen::Matrix<T,3,3>::Zero();
-  G2_(0,2) = 1.;
-  G2_(2,0) = -1.;
-  return G2_;
-}
-
-template<typename T>
-Eigen::Matrix<T,3,3> SO3<T>::G3() {
-  static Eigen::Matrix<T,3,3> G3_ = Eigen::Matrix<T,3,3>::Zero();
-  G3_(1,0) = 1.;
-  G3_(0,1) = -1.;
-  return G3_;
-}
-
-template<typename T>
-SO3<T> SO3<T>::Random() {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::normal_distribution<T> normal(0,1);
-  std::uniform_real_distribution<T> unif(0,M_PI);
-  // sample uniformly random point inside the sphere of radius pi in
-  // the axes angle space and map it back to a rotation.
-  Eigen::Matrix<T,3,1> w(normal(gen), normal(gen), normal(gen));
-  w *= unif(gen)/w.norm();
-  return SO3<T>(Exp_(w));
-}
-
-// Not sure about those rotations
-template<typename T>
-SO3<T> SO3<T>::Rx(T alpha) { 
-  Eigen::Matrix<T,3,3> R;
-  R << 1., 0., 0.,
-       0., cos(alpha), -sin(alpha),
-       0., sin(alpha), cos(alpha);
-  return SO3<T>(R);
-}
-template<typename T>
-SO3<T> SO3<T>::Ry(T alpha) {
-  Eigen::Matrix<T,3,3> R;
-  R << cos(alpha), 0., sin(alpha),
-       0.,         1.,    0.,
-       -sin(alpha),0., cos(alpha);
-  return SO3<T>(R);
-}
-template<typename T>
-SO3<T> SO3<T>::Rz(T alpha) {
-  Eigen::Matrix<T,3,3> R;
-  R << cos(alpha), -sin(alpha), 0.,
-       sin(alpha), cos(alpha), 0.,
-       0.,          0.,         1.;
-  return SO3<T>(R);
+template<typename T, int Options>
+SO3<T,Options> SO3<T,Options>::Rz(T alpha) {
+  return SO3<T,Options>(Eigen::Matrix<T,3,1,Options>(0.,0.,1.), alpha);
 }
 
 }
