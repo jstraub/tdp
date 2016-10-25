bb421a0c4b1fab9e16e8f20bf214527de3ee1394
Julian Straub <jstraub@csail.mit.edu>
2016-10-24 21:59:01 -0400

    draw poses

--- include/tdp/gl/gl_draw.h
+++ include/tdp/gl/gl_draw.h
@@ -1,13 +1,29 @@
 
 #pragma once
 
+#include <vector>
 #include <pangolin/gl/gldraw.h>
+#include <tdp/manifold/SE3.h>
 
 namespace tdp {
 void glDrawPoint(GLfloat x1, GLfloat y1) {
   glBegin(GL_POINTS); // render with points
   glVertex2f(x1,y1); //display a point
   glEnd();
+}
+
+void glDrawPoses(const std::vector<SE3f>& Ts, int step=10, float scale=0.1) {
+  for (size_t i=1; i<Ts.size(); ++i) {
+    pangolin::glDrawLine(
+        Ts[i].translation()(0),
+        Ts[i].translation()(1),
+        Ts[i].translation()(2),
+        Ts[i-1].translation()(0),
+        Ts[i-1].translation()(1),
+        Ts[i-1].translation()(2));
+  }
+  for (size_t i=0; i<Ts.size(); i+=step)
+    pangolin::glDrawAxis(Ts[i].matrix(), scale);
 }
 
 }
