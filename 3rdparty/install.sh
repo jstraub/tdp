710070eb418f70d94c51a3c1928041c8cc3965c9
Julian Straub <jstraub@csail.mit.edu>
2016-11-01 17:50:43 -0400

    adding Spectra

--- 3rdparty/install.sh
+++ 3rdparty/install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+
 cd ./ann_1.1.2/
 mkdir lib
 make linux-g++
---
+++ test/spectra.cpp
@@ -0,0 +1,76 @@
+#include <tdp/testing/testing.h>
+#include <iostream>
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include <SymEigsSolver.h>
+
+#include <GenEigsSolver.h>
+#include <MatOp/SparseGenMatProd.h>
+
+
+TEST(Spectra, dense) {
+  // We are going to calculate the eigenvalues of M
+  Eigen::MatrixXd A = Eigen::MatrixXd::Random(10, 10);
+  Eigen::MatrixXd M = A + A.transpose();
+
+  // Construct matrix operation object using the wrapper class DenseGenMatProd
+  Spectra::DenseSymMatProd<double> op(M);
+
+  // Construct eigen solver object, requesting the largest three eigenvalues
+  Spectra::SymEigsSolver< double, 
+    Spectra::LARGEST_ALGE, Spectra::DenseSymMatProd<double> > eigs(&op, 3, 6);
+
+  // Initialize and compute
+  eigs.init();
+  int nconv = eigs.compute();
+
+  // Retrieve results
+  Eigen::VectorXd evalues;
+  if(eigs.info() == Spectra::SUCCESSFUL)
+    evalues = eigs.eigenvalues();
+
+  std::cout << "Eigenvalues found:\n" << evalues << std::endl;
+}
+
+TEST(Spectra, sparse) {
+  // A band matrix with 1 on the main diagonal, 2 on the below-main subdiagonal,
+  // and 3 on the above-main subdiagonal
+  const int n = 1000000;
+  Eigen::SparseMatrix<double> M(n, n);
+  M.reserve(Eigen::VectorXi::Constant(n, 3));
+  for(int i = 0; i < n; i++)
+  {
+    M.insert(i, i) = 1.0;
+    if(i > 0)
+      M.insert(i - 1, i) = 3.0;
+    if(i < n - 1)
+      M.insert(i + 1, i) = 2.0;
+  }
+
+  // Construct matrix operation object using the wrapper class SparseGenMatProd
+  Spectra::SparseGenMatProd<double> op(M);
+
+  // Construct eigen solver object, requesting the largest three eigenvalues
+  Spectra::GenEigsSolver< double, Spectra::SMALLEST_MAGN,
+    Spectra::SparseGenMatProd<double> > eigs(&op, 3, 6);
+
+  // Initialize and compute
+  eigs.init();
+  int nconv = eigs.compute();
+
+  // Retrieve results
+  Eigen::VectorXcd evalues;
+  if(eigs.info() == Spectra::SUCCESSFUL) {
+    evalues = eigs.eigenvalues();
+    std::cout << "Eigenvalues found:\n" << evalues << std::endl;
+//    std::cout << eigs.eigenvectors() << std::endl;
+  }
+
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
