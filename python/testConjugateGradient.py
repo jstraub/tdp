2ef6c4fc3b21ec493ef35277aaf22ceb1affb0cc
Julian Straub <jstraub@csail.mit.edu>
2016-11-04 16:31:30 -0400

    preconditioned CG

--- python/testConjugateGradient.py
+++ python/testConjugateGradient.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.linalg import solve
+from scipy.linalg import solve, inv
 
 def solveCG(ATA,ATb,x0):
   x = np.copy(x0)
@@ -37,6 +37,26 @@ def solveCG_direct(A,b,x0):
     rsold = rsnew
   return x
 
+def solvePCG_direct(A,b,x0,M):
+  x = np.copy(x0)
+  r = A.T.dot(b-A.dot(x))
+  z = solve(M,r)
+  p = np.copy(r)
+  rsold = r.T.dot(z)
+  for i in range(1000):
+    Ap = A.T.dot(A.dot(p))
+    alpha = rsold/(p.T.dot(Ap))
+    x += alpha*p
+    r -= alpha*Ap
+    if np.sqrt(r.T.dot(r)) < 1e-10:
+      print i
+      break
+    z = solve(M,r)
+    rsnew = r.T.dot(z)
+    p = z+(rsnew/rsold)*p
+    rsold = rsnew
+  return x
+
 A = np.random.randn(100,12)
 b = np.random.randn(100,1)
 
@@ -45,8 +65,16 @@ ATb = A.T.dot(b)
 
 x = solve(ATA,ATb)
 print x.T
+
 x = solveCG(ATA,ATb,np.zeros((12,1)))
 print x.T
+
 x = solveCG_direct(A,b,np.zeros((12,1)))
 print x.T
 
+x = solvePCG_direct(A,b,np.zeros((12,1)),np.eye(12))
+print x.T
+
+M = np.diag(np.diag(A))
+x = solvePCG_direct(A,b,np.zeros((12,1)),M)
+print x.T
