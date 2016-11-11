46518b7447d7354cb0a5eea537fafeef9af75bce
Hayley Song <hjsong@mit.edu>
2016-11-10 20:07:33 -0500

    adds views of two spheres

--- experiments/fMap/main.cpp
+++ experiments/fMap/main.cpp
@@ -55,29 +55,28 @@
 
 int main( int argc, char* argv[] ){
 
-    std::srand(101);
-    //test_Laplacian();
-   //return 1;
-  // load pc and normal from the input paths
-  tdp::ManagedHostImage<tdp::Vector3fda> pc_s(10000,1);
-  tdp::ManagedHostImage<tdp::Vector3fda> ns_s(10000,1);
+  tdp::ManagedHostImage<tdp::Vector3fda> pc_s(1000,1);
+  tdp::ManagedHostImage<tdp::Vector3fda> ns_s(1000,1);
 
-  tdp::ManagedHostImage<tdp::Vector3fda> pc_t(10000,1);
-  tdp::ManagedHostImage<tdp::Vector3fda> ns_t(10000,1);
+  tdp::ManagedHostImage<tdp::Vector3fda> pc_t(1000,1);
+  tdp::ManagedHostImage<tdp::Vector3fda> ns_t(1000,1);
 
   if (argc > 1) {
       const std::string input = std::string(argv[1]);
       std::cout << "input pc: " << input << std::endl;
-      tdp::LoadPointCloud(input, pc_s, ns);
+      tdp::LoadPointCloud(input, pc_s, ns_s);
   } else {
+      std::srand(101);
       GetSphericalPc(pc_s);
+      std::srand(200);
+      GetSphericalPc(pc_t);
       //GetCylindricalPc(pc);
   }
 
   // build kd tree
   tdp::ANN ann_s, ann_t;
   ann_s.ComputeKDtree(pc_s);
-  ann_t.ComputeKDtree(pc_s);
+  ann_t.ComputeKDtree(pc_t);
 
   // Create OpenGL window - guess sensible dimensions
   int menue_w = 180;
@@ -102,37 +101,37 @@ int main( int argc, char* argv[] ){
   pangolin::View& viewPc = pangolin::CreateDisplay()
     .SetHandler(new pangolin::Handler3D(s_cam));
   container.AddDisplay(viewPc);
-  pangolin::View& viewN = pangolin::CreateDisplay()
+  pangolin::View& viewPc_t = pangolin::CreateDisplay()
     .SetHandler(new pangolin::Handler3D(s_cam));
-  container.AddDisplay(viewN);
+  container.AddDisplay(viewPc_t);
+
 
   // use those OpenGL buffers
-  pangolin::GlBuffer vbo, vboM, vboS, vboF, valuebo;
+  pangolin::GlBuffer vbo,vbo_t, valuebo_s,valuebo_t;
   vbo.Reinitialise(pangolin::GlArrayBuffer, pc_s.Area(),  GL_FLOAT, 3, GL_DYNAMIC_DRAW);
   vbo.Upload(pc_s.ptr_, pc_s.SizeBytes(), 0);
+  vbo_t.Reinitialise(pangolin::GlArrayBuffer, pc_t.Area(),  GL_FLOAT, 3, GL_DYNAMIC_DRAW);
+  vbo_t.Upload(pc_t.ptr_, pc_t.SizeBytes(), 0);
 
   // Add variables to pangolin GUI
   pangolin::Var<bool> showFMap("ui.show fMap", true, false);
-//  pangolin::Var<int> pcOption("ui. pc option", 0, 0,1);
+  // pangolin::Var<int> pcOption("ui. pc option", 0, 0,1);
   // variables for KNN
   pangolin::Var<int> knn("ui.knn",30,1,100);
   pangolin::Var<float> eps("ui.eps", 1e-6 ,1e-7, 1e-5);
-
-  pangolin::Var<int> idEv("ui.id EV", 100, 0, 150);
   pangolin::Var<float> alpha("ui. alpha", 0.01, 0.005, 0.3); //variance of rbf kernel
+  pangolin::Var<int> numEv("ui.numEv",3,1,300);
+
   // viz color coding
   pangolin::Var<float>minVal("ui. min Val",-0.71,-1,0);
   pangolin::Var<float>maxVal("ui. max Val",0.01,1,0);
-
+  float minVal_t, maxVal_t;
 
   Eigen::SparseMatrix<float> L_s(pc_s.Area(), pc_s.Area());
   Eigen::SparseMatrix<float> L_t(pc_t.Area(), pc_t.Area());
-  Eigen::
-  Eigen::VectorXf evector(pc_s.Area(),1);
-  Eigen::MatrixXf curvature(pc_s.Area(),3);
+  tdp::eigen_vector<Eigen::VectorXf> basis_s(numEv, Eigen::VectorXf::Zero(L_s.rows()));
+  tdp::eigen_vector<Eigen::VectorXf> basis_t(numEv, Eigen::VectorXf::Zero(L_s.rows()));
 
-  tdp::eigen_vector<tdp::Vector3fda> means;
-  means.reserve(nBins);
   // Stream and display video
   while(!pangolin::ShouldQuit())
   {
@@ -140,56 +139,35 @@ int main( int argc, char* argv[] ){
     glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
     glColor3f(1.0f, 1.0f, 1.0f);
 
-    if (pangolin::Pushed(runSkinning) || knn.GuiChanged() || upsample.GuiChanged()
-            || idEv.GuiChanged() || alpha.GuiChanged()) {
-        //  processing of PC for skinning
-      std::cout << "Running skinning..." << std::endl;
-
-      if( pcOption.GuiChanged()){
-        std::cout << "Roading new pc..." << pcOption << std::endl;
-        if (pcOption == 0){
-            GetSphericalPc(pc_s);
-        } else if (pcOption ==1 ){
-            GetCylindricalPc(pc_s);
-        }
-      }
+    if (pangolin::Pushed(showFMap) || knn.GuiChanged() || alpha.GuiChanged() ||
+            numEv.GuiChanged()){
+      std::cout << "Running fMap..." << std::endl;
 
       // get Laplacian operator and its eigenvectors
       tdp::Timer t0;
-      L = getLaplacian(pc_s, ann_s, knn, eps, alpha);
-      t0.toctic("GetLaplacian");
-      evector = getLaplacianEvector(pc_s, L, idEv);
-      t0.toctic("GetEigenVector");
+      L_s = tdp::getLaplacian(pc_s, ann_s, knn, eps, alpha);
+      L_t = tdp::getLaplacian(pc_t, ann_t, knn, eps, alpha);
+      t0.toctic("GetLaplacians");
+      tdp::getLaplacianEvectors(L_s, numEv, basis_s);
+      tdp::getLaplacianEvectors(L_t, numEv, basis_t);
+      t0.toctic("GetEigenVectors");
 
       // color-coding on the surface
-      valuebo.Reinitialise(pangolin::GlArrayBuffer, evector.rows(),
-                             GL_FLOAT,1, GL_DYNAMIC_DRAW);
-      valuebo.Upload(&evector(0), sizeof(float)*evector.rows(), 0);
-      std::cout << evector.minCoeff() << " " << evector.maxCoeff() << std::endl;
-      minVal = evector.minCoeff()-1e-3;
-      maxVal = evector.maxCoeff();
+      valuebo_s.Reinitialise(pangolin::GlArrayBuffer, basis_s[1].rows(),GL_FLOAT,1, GL_DYNAMIC_DRAW);
+      valuebo_s.Upload(&basis_s[1](0), sizeof(float)*basis_s[1].rows(), 0);
+      std::cout << basis_s[1].minCoeff() << " " << basis_s[1].maxCoeff() << std::endl;
+      minVal = basis_s[1].minCoeff()-1e-3;
+      maxVal = basis_s[1].maxCoeff();
 
-      //curvature = getMeanCurvature(pc, L);
+      valuebo_t.Reinitialise(pangolin::GlArrayBuffer, basis_t[1].rows(),GL_FLOAT,1, GL_DYNAMIC_DRAW);
+      valuebo_t.Upload(&basis_t[1](0), sizeof(float)*basis_t[1].rows(), 0);
+      std::cout << basis_t[1].minCoeff() << " " << basis_t[1].maxCoeff() << std::endl;
+      minVal_t = basis_t[1].minCoeff()-1e-3;
+      maxVal_t = basis_t[1].maxCoeff();
 
-      //Decompose curvature to get normals and mean curvature value
-      //Eigen::VectorXf meanCurvature = curvature.rowwise().norm();
-      //std::cout << "meanCurvature of a row: " << meanCurvature.transpose() << std::endl;
-
-      std::cout << "<--DONE skinning-->" << std::endl;
-      recomputeMeans = true;
+      std::cout << "<--DONE fMap-->" << std::endl;
     }
 
-    if (pangolin::Pushed(recomputeMeans) || nBins.GuiChanged()) {
-
-        // Get the means of the level sets
-        means = getLevelSetMeans(pc_s, evector, nBins);
-        for (int i=0; i<means.size(); ++i){
-            if(means[i](0)>1 || means[i](1) >1 || means[i](2)>1){
-                std::cout << "OH NO! this should never be printed!: \n" << means[i].transpose() << std::endl;
-            }
-        }
-        std::cout << std::endl;
-    }
 
     // Draw 3D stuff
     glEnable(GL_DEPTH_TEST);
@@ -198,30 +176,8 @@ int main( int argc, char* argv[] ){
       viewPc.Activate(s_cam);
       pangolin::glDrawAxis(0.1);
 
-      if (showBases) {
-          for (size_t i=0; i<T_wls.Area(); ++i) {
-              pangolin::glDrawAxis(T_wls[i].matrix(), 0.05f);
-          }
-      }
-
       glPointSize(2.);
       glColor3f(1.0f, 1.0f, 0.0f);
-      pangolin::RenderVbo(vboF);
-
-      glColor3f(.3,1.,.125);
-      glLineWidth(2);
-      tdp::Vector3fda m, m_prev;
-      for (size_t i=1; i<means.size(); ++i){
-          m_prev = means[i-1];
-          m = means[i];
-          tdp::glDrawLine(m_prev, m);
-      }
-
-      //glPointSize(1.);
-      // draw the first arm pc
-      //glColor3f(1.0f, 0.0f, 0.0f);
-      //pangolin::RenderVbo(vbo);
-
 
       // renders the vbo with colors from valuebo
       auto& shader = tdp::Shaders::Instance()->valueShader_;
@@ -230,7 +186,7 @@ int main( int argc, char* argv[] ){
       shader.SetUniform("MV", s_cam.GetModelViewMatrix());
       shader.SetUniform("minValue", minVal);
       shader.SetUniform("maxValue", maxVal);
-      valuebo.Bind();
+      valuebo_s.Bind();
       glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 0, 0);
       vbo.Bind();
       glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
@@ -241,10 +197,39 @@ int main( int argc, char* argv[] ){
       glDrawArrays(GL_POINTS, 0, vbo.num_elements);
       shader.Unbind();
       glDisableVertexAttribArray(1);
-      valuebo.Unbind();
+      valuebo_s.Unbind();
       glDisableVertexAttribArray(0);
       vbo.Unbind();
+    }
 
+    if (viewPc_t.IsShown()){
+        viewPc_t.Activate(s_cam);
+        pangolin::glDrawAxis(0.1);
+
+        glPointSize(2.);
+        glColor3f(1.0f, 1.0f, 0.0f);
+
+        // renders the vbo with colors from valuebo
+        auto& shader_t = tdp::Shaders::Instance()->valueShader_;
+        shader_t.Bind();
+        shader_t.SetUniform("P",  s_cam.GetProjectionMatrix());
+        shader_t.SetUniform("MV", s_cam.GetModelViewMatrix());
+        shader_t.SetUniform("minValue", minVal_t);
+        shader_t.SetUniform("maxValue", maxVal_t);
+        valuebo_t.Bind();
+        glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 0, 0);
+        vbo_t.Bind();
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+        glPointSize(4.);
+        glDrawArrays(GL_POINTS, 0, vbo_t.num_elements);
+        shader_t.Unbind();
+        glDisableVertexAttribArray(1);
+        valuebo_t.Unbind();
+        glDisableVertexAttribArray(0);
+        vbo_t.Unbind();
 
     }
 
