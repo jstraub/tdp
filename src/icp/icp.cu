21c896ade6f09104e92416be7a070fb665c4bb39
Julian Straub <jstraub@csail.mit.edu>
2016-11-30 11:18:31 -0500

    new right side ICP A matrix computation; not sure if its working

--- src/icp/icp.cu
+++ src/icp/icp.cu
@@ -183,6 +183,7 @@ __global__ void KernelICPStep(
       // found association -> check thresholds;
       Vector3fda n_o_in_m = T_mo.rotation()*n_o(u,v);
       Vector3fda n_mi = n_m(x,y);
+      Vector3fda n_m_in_oi = T_mo.rotation().Inverse()*n_mi;
       Vector3fda pc_mi = pc_m(x,y);
       Vector3fda pc_oi = pc_o(u,v);
       Vector3fda pc_o_in_m = T_mo * pc_oi;
@@ -199,8 +200,12 @@ __global__ void KernelICPStep(
 //        top = (pc_o_in_m).cross(n_mi);
 //        bottom = n_mi;
         // right multiplication of error
-        top = (pc_oi).cross(T_mo.rotation().Inverse()*n_mi);
-        bottom = n_mi;
+        top = (pc_oi).cross(n_m_in_oi);
+        bottom = n_m_in_oi;
+        float nTt = n_mi.dot(T_mo.translation());
+        top.array() += nTt;
+        bottom.array() += nTt;
+
         ab[6] = n_mi.dot(pc_mi-pc_o_in_m);
         Eigen::Matrix<float,29,1,Eigen::DontAlign> upperTriangle;
         int k=0;
