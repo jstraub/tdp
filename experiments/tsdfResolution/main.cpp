fb69ae04052e4d3fff9473a5cef5513770a46a92
Erik Nguyen <enguyen@mit.edu>
2016-11-30 15:42:31 -0500

    Refactored the TSDF resolution experiment
    * Moved the point cloud generation and related methods to
      the tsdf_shapes.h class to be reused during the volume calculation
      experiments
    
    * Fixed the point cloud and tsdf generation to be able to set
      arbitrary scales. This helps the tsdf have better resolution
      with the same discretization because if cross-sectional areas
      are much smaller than lateral dimensions, we can fit the scales
      to more snuggly fit the underlying object

--- experiments/tsdfResolution/main.cpp
+++ experiments/tsdfResolution/main.cpp
@@ -36,139 +36,14 @@
 #include <tdp/io/tinyply.h>
 #include <tdp/gl/shaders.h>
 
+#include <tdp/tsdf/tsdf_shapes.h>
+
 #include <math.h>
 #include <cmath>
 #include <stdlib.h>
 
-/*
- *  Generate a cylindrical model of the arm. Assume the radius of the arm
- * is on average 1/10 the length of the arm. The model of the arm will be
- * a cylinder with rotational axis on the z axis centered at the origin.
- * The hieght of the cylinder will span from -0.75 to 0.75 units. Hopefully
- * this will somewhat simulate the viewpoint we would have on the arm.
- */
-float make_cylindrical_point_cloud(Eigen::Matrix<float, 3, Eigen::Dynamic>& points)
-{
-  const float PI = 3.1415927f;
-  const float HEIGHT_SCALE = 0.75f;
-  const float RADIUS = 2 * HEIGHT_SCALE / 10;
-
-  // Note that the area of the lateral surface of the cylinder to each of the
-  // circular faces is 20 : 1 : 1, therefore the first unit of randomness can
-  // either determine the height of the point, or if it should be part of the
-  // circular faces, it determines the radius from the center of the point
-  for (size_t i = 0; i < points.cols(); i++) {
-    // Random returns a number from [-1, 1] for each index
-    Eigen::Vector2f random = Eigen::Vector2f::Random();
-    float x, y, z, theta;
-
-    if (i % 22 == 0) {                          // Top face
-      z = HEIGHT_SCALE;
-      x = random(0) * RADIUS;
-      y = random(1) * RADIUS;
-
-      while (x * x + y * y > RADIUS * RADIUS) {
-        random = Eigen::Vector2f::Random();
-        x = random(0) * RADIUS;
-        y = random(1) * RADIUS;
-      }
-    } else if (i % 22 == 1) {                   // Bottom face
-      z = -HEIGHT_SCALE;
-      x = random(0) * RADIUS;
-      y = random(1) * RADIUS;
-
-      while (x * x + y * y > RADIUS * RADIUS) {
-        random = Eigen::Vector2f::Random();
-        x = random(0) * RADIUS;
-        y = random(1) * RADIUS;
-      }
-    } else {                                    // Lateral surface the other 20 times
-      z = random(0) * HEIGHT_SCALE;
-      theta = random(1) * PI;
-      x = RADIUS * cos(theta);
-      y = RADIUS * sin(theta);
-    }
-
-    points(0, i) = x + 1;
-    points(1, i) = y + 1;
-    points(2, i) = z + 1;
-  }
-
-  return PI * RADIUS * RADIUS * 2 * HEIGHT_SCALE;
-}
-
-static inline void setValue(tdp::ManagedHostVolume<tdp::TSDFval>&tsdf, int i, int j, int k, float f) {
-  tsdf(i, j, k).f = f;
-  tsdf(i, j, k).w = 2;
-  tsdf(i, j, k).r = 128;
-  tsdf(i, j, k).g = 128;
-  tsdf(i, j, k).b = 128;
-}
-
-static inline float outside_cylinder(float x, float y, float z, float cx, float cy, float cz) {
-  const float MAX_Z = 0.75f;
-  const float MAX_R = 2 * MAX_Z / 10;
-
-  x -= cx;
-  y -= cy;
-  z -= cz;
-
-  return z <= -MAX_Z || z >= MAX_Z || sqrt(x * x + y * y) >= MAX_R;
-}
-
-void build_tsdf(tdp::ManagedHostVolume<tdp::TSDFval>& tsdf, Eigen::Matrix<float, 3, Eigen::Dynamic>& points, float xScale, float yScale, float zScale)
-{
-  // if there are n points in each direction numbered from [0, n-1] that need to map to [-1, 1],
-  // then we can map the coordinates by doing (2i / (n - 1) - 1)
-  // Note that we need to prevent points inside the surface from being anything but -1
-
-  tdp::Image<tdp::Vector3fda> pc(points.cols(),1,(tdp::Vector3fda*)&(points(0,0)));
-
-  tdp::ANN ann;
-  ann.ComputeKDtree(pc);
-  Eigen::VectorXi nnIds(1);
-  Eigen::VectorXf dists(1);
-
-  for (int i = 0; i < tsdf.w_; i++) {
-    float x = xScale * i;
-    
-    for (int j = 0; j < tsdf.h_; j++) {
-      float y = yScale * j;
-      
-      for (int k = 0; k < tsdf.d_; k++) {
-        float z = zScale * k;
-
-        float f;
-        tdp::Vector3fda q(x, y, z);
-        ann.Search(q, 1, 1e-7, nnIds, dists);
-        f = sqrt(dists(0));
-        if (!outside_cylinder(x, y, z, 1, 1, 1)) {
-          f *= -1.0f;
-        }
-
-        setValue(tsdf, i, j, k, f);
-      }
-    }
-  }
-}
-
-void build_sphere_tsdf(tdp::ManagedHostVolume<tdp::TSDFval>& tsdf, float r, int n) {
-  for (int i = 0; i < tsdf.w_; i++) {
-    for (int j = 0; j < tsdf.h_; j++) {
-      for (int k = 0; k < tsdf.d_; k++) {
-        float x = (i - n / 2);
-        float y = (j - n / 2);
-        float z = (k - n / 2);
-
-        float f = sqrt(x * x + y * y + z * z) - r;
-
-        setValue(tsdf, i, j, k, f);
-      }
-    }
-  }
-}
-
 void calculate_volumes(Eigen::Matrix<float, 3, Eigen::Dynamic>& points,
+                      float *boundingLength, float* center,
                       int min, int max, int step, float expectedVolume) {
   std::cout << "Expected: " << expectedVolume << std::endl;
   // cylindrical tsdf
@@ -176,12 +51,14 @@ void calculate_volumes(Eigen::Matrix<float, 3, Eigen::Dynamic>& points,
 
   for (int discretization = min; discretization <= max; discretization += step) {
     tdp::ManagedHostVolume<tdp::TSDFval> tsdf(discretization, discretization, discretization);
-    float xScale = 2.0f / (discretization - 1);
-    float yScale = 2.0f / (discretization - 1);
-    float zScale = 2.0f / (discretization - 1);
+    float scale[3] = {
+      boundingLength[0] / (discretization - 1),
+      boundingLength[1] / (discretization - 1),
+      boundingLength[2] / (discretization - 1)
+    };
 
-    build_tsdf(tsdf, points, xScale, yScale, zScale);
-    surface.GenerateSurface(&tsdf, 0.0f, xScale, yScale, zScale, 1.0f, 1.0f);
+    tdp::TsdfShapeFields::build_tsdf(tsdf, points, scale, center);
+    surface.GenerateSurface(&tsdf, 0.0f, scale[0], scale[1], scale[2], 1.0f, 1.0f);
     if (!surface.IsSurfaceValid()) {
       pango_print_error("Unable to generate surface");
     }
@@ -197,22 +74,28 @@ int main( int argc, char* argv[] )
   // Generate the same point cloud each time
   srand(0);
   int num_points = 22 * 1000;
+  float boundingLength  [3];
+  float center          [3];
   Eigen::Matrix<float, 3, Eigen::Dynamic> points(3, num_points);
-  float volume = make_cylindrical_point_cloud(points);
+  float volume = tdp::TsdfShapeFields::make_cylindrical_point_cloud(points, boundingLength, center);
   std::cout << "Built Point Cloud" << std::endl;
 
-  calculate_volumes(points, 12, 512, 4, volume);
-  /*
-  int discretization = 12;
-
+  //calculate_volumes(points, boundingLength, center, 128, 128, 4, volume);
+  
+  int discretization = 128;
+  float scale[3] = {
+    boundingLength[0] / (discretization - 1),
+    boundingLength[1] / (discretization - 1),
+    boundingLength[2] / (discretization - 1)
+  };
 
   // cylindrical tsdf
   tdp::ManagedHostVolume<tdp::TSDFval> tsdf(discretization, discretization, discretization);
-  float xScale = 2.0f / (discretization - 1);
-  float yScale = 2.0f / (discretization - 1);
-  float zScale = 2.0f / (discretization - 1);
+  float xScale = scale[0];
+  float yScale = scale[1];
+  float zScale = scale[2];
 
-  build_tsdf(tsdf, points, xScale, yScale, zScale);
+  tdp::TsdfShapeFields::build_tsdf(tsdf, points, scale, center);
   std::cout << "Finished building TSDF" << std::endl;
 
   // Create OpenGL window - guess sensible dimensions
@@ -394,6 +277,6 @@ int main( int argc, char* argv[] )
     pangolin::FinishFrame();
   }
   return 0;
-  */
+  
 }
 
---
+++ include/tdp/tsdf/tsdf_shapes.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include <tdp/tsdf/tsdf.h>
+#include <tdp/data/managed_image.h>
+#include <tdp/data/managed_volume.h>
+#include <tdp/eigen/dense.h>
+#include <tdp/nn/ann.h>
+
+#include <math.h>
+#include <cmath>
+
+namespace tdp {
+
+struct TsdfShapeFields {
+
+  /*
+   *  Generate a cylindrical model of the arm. Assume the radius of the arm
+   * is on average 1/10 the length of the arm. The model of the arm will be
+   * a cylinder with rotational axis on the z axis centered at the origin.
+   */
+  static float make_cylindrical_point_cloud(Eigen::Matrix<float, 3, Eigen::Dynamic>& points, float* boundingLength, float* center)
+  {
+    const float PI = 3.1415927f;
+    const float HEIGHT_SCALE = 0.75f;
+    const float RADIUS = 2 * HEIGHT_SCALE / 10;
+
+    // The dimensions of the box that would completely surround the cylinder
+    boundingLength[0] = 0.4f;
+    boundingLength[1] = 0.4f;
+    boundingLength[2] = 2.0f;
+
+    // The center of the cylinder. Not that this allows the marching cubes
+    // reconstruction to place the bounding box at the origin.
+    center[0] = 0.2f;
+    center[1] = 0.2f;
+    center[2] = 1.0f;
+
+    // Note that the area of the lateral surface of the cylinder to each of the
+    // circular faces is 20 : 1 : 1, therefore the first unit of randomness can
+    // either determine the height of the point, or if it should be part of the
+    // circular faces, it determines the radius from the center of the point
+    for (size_t i = 0; i < points.cols(); i++) {
+      // Random returns a number from [-1, 1] for each index
+      Eigen::Vector2f random = Eigen::Vector2f::Random();
+      float x, y, z, theta;
+
+      if (i % 22 == 0) {                          // Top face
+        z = HEIGHT_SCALE;
+        x = random(0) * RADIUS;
+        y = random(1) * RADIUS;
+
+        while (x * x + y * y > RADIUS * RADIUS) {
+          random = Eigen::Vector2f::Random();
+          x = random(0) * RADIUS;
+          y = random(1) * RADIUS;
+        }
+      } else if (i % 22 == 1) {                   // Bottom face
+        z = -HEIGHT_SCALE;
+        x = random(0) * RADIUS;
+        y = random(1) * RADIUS;
+
+        while (x * x + y * y > RADIUS * RADIUS) {
+          random = Eigen::Vector2f::Random();
+          x = random(0) * RADIUS;
+          y = random(1) * RADIUS;
+        }
+      } else {                                    // Lateral surface the other 20 times
+        z = random(0) * HEIGHT_SCALE;
+        theta = random(1) * PI;
+        x = RADIUS * cos(theta);
+        y = RADIUS * sin(theta);
+      }
+
+      points(0, i) = x + center[0];
+      points(1, i) = y + center[1];
+      points(2, i) = z + center[2];
+    }
+
+    return PI * RADIUS * RADIUS * 2 * HEIGHT_SCALE;
+  }
+
+  static inline void set_value(tdp::ManagedHostVolume<tdp::TSDFval>&tsdf, int i, int j, int k, float f) {
+    tsdf(i, j, k).f = f;
+    tsdf(i, j, k).w = 2;
+    tsdf(i, j, k).r = 128;
+    tsdf(i, j, k).g = 128;
+    tsdf(i, j, k).b = 128;
+  }
+
+  static inline float outside_cylinder(float x, float y, float z, float* center) {
+    const float MAX_Z = 0.75f;
+    const float MAX_R = 2 * MAX_Z / 10;
+
+    x -= center[0];
+    y -= center[1];
+    z -= center[2];
+
+    return z <= -MAX_Z || z >= MAX_Z || sqrt(x * x + y * y) >= MAX_R;
+  }
+
+  static void build_tsdf(tdp::ManagedHostVolume<tdp::TSDFval>& tsdf, Eigen::Matrix<float, 3, Eigen::Dynamic>& points, float* scale, float* center)
+  {
+    // if there are n points in each direction numbered from [0, n-1] that need to map to [-1, 1],
+    // then we can map the coordinates by doing (2i / (n - 1) - 1)
+    // Note that we need to prevent points inside the surface from being anything but -1
+
+    tdp::Image<tdp::Vector3fda> pc(points.cols(),1,(tdp::Vector3fda*)&(points(0,0)));
+
+    tdp::ANN ann;
+    ann.ComputeKDtree(pc);
+    Eigen::VectorXi nnIds(1);
+    Eigen::VectorXf dists(1);
+
+    float mid_x = (tsdf.w_ - 1) / 2.0f;
+    float mid_y = (tsdf.h_ - 1) / 2.0f;
+    float mid_z = (tsdf.d_ - 1) / 2.0f;
+
+    float xScale = scale[0];
+    float yScale = scale[1];
+    float zScale = scale[2];
+
+    for (int i = 0; i < tsdf.w_; i++) {
+      float x = xScale * (i - mid_x) + center[0];
+      
+      for (int j = 0; j < tsdf.h_; j++) {
+        float y = yScale * (j - mid_y) + center[1];
+        
+        for (int k = 0; k < tsdf.d_; k++) {
+          float z = zScale * (k - mid_z) + center[2];
+
+          float f;
+          tdp::Vector3fda q(x, y, z);
+          ann.Search(q, 1, 1e-7, nnIds, dists);
+          f = sqrt(dists(0));
+          if (!outside_cylinder(x, y, z, center)) {
+            f *= -1.0f;
+          }
+
+          set_value(tsdf, i, j, k, f);
+        }
+      }
+    }
+  }
+};
+
+}
