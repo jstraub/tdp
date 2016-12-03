3b3bb6f8a3bf549ba77462aa5d9d280c1846f94b
Julian Straub <jstraub@csail.mit.edu>
2016-12-03 16:44:55 -0500

    adding rendervboibocbo

--- include/tdp/gl/render.h
+++ include/tdp/gl/render.h
@@ -32,4 +32,35 @@ void RenderLabeledVbo(
   vbo.Unbind();
 }
 
+void RenderVboIbo(
+  pangolin::GlBuffer& vbo,
+  pangolin::GlBuffer& ibo
+    ) {
+	vbo.Bind();
+	glVertexPointer(vbo.count_per_element, vbo.datatype, 0, 0);
+	glEnableClientState(GL_VERTEX_ARRAY);
+
+	ibo.Bind();
+	glDrawElements(GL_TRIANGLES,ibo.num_elements*3, ibo.datatype, 0);
+	ibo.Unbind();
+
+	glDisableClientState(GL_VERTEX_ARRAY);
+	vbo.Unbind();
+}
+
+void RenderVboIboCbo(
+  pangolin::GlBuffer& vbo,
+  pangolin::GlBuffer& ibo,
+  pangolin::GlBuffer& cbo
+    ) {
+	cbo.Bind();
+	glColorPointer(cbo.count_per_element, cbo.datatype, 0, 0);
+	glEnableClientState(GL_COLOR_ARRAY);
+
+	tdp::RenderVboIbo(vbo,ibo);
+
+	glDisableClientState(GL_COLOR_ARRAY);
+	cbo.Unbind();
+}
+
 }
