de6b260600c8a9ec547437b549c7cd451d374544
Julian Straub <jstraub@csail.mit.edu>
2016-12-12 15:31:31 -0500

    fixed pyramid generation for masks

--- include/tdp/preproc/mask.h
+++ include/tdp/preproc/mask.h
@@ -12,12 +12,30 @@ void ApplyMask(const Image<uint8_t>& mask, Image<T>& I);
 
 template<typename T, int LEVELS>
 void ApplyMask(const Pyramid<uint8_t,LEVELS>& mask, Pyramid<T,LEVELS>& I) {
-  for (int lvl=1; lvl<LEVELS; ++lvl) {
+  for (int lvl=0; lvl<LEVELS; ++lvl) {
     Image<uint8_t> maskLvl = mask.GetConstImage(lvl);
     Image<T> Ilvl = I.GetImage(lvl);
 		ApplyMask<T>(maskLvl, Ilvl);
 	}
 }
 
+void PyrDownMask(const Image<uint8_t>& Iin, Image<uint8_t>& Iout);
+
+template<int LEVELS>
+void ConstructPyramidFromMask(const Image<uint8_t>& mask, 
+    Pyramid<uint8_t,LEVELS>& P) {
+  P.GetImage(0).CopyFrom(mask);
+  CompleteMaskPyramid(P);
+}
+
+template<int LEVELS>
+void CompleteMaskPyramid(Pyramid<uint8_t,LEVELS>& P) {
+  // P is on GPU so perform downsampling on GPU
+  for (int lvl=1; lvl<LEVELS; ++lvl) {
+    Image<uint8_t> Isrc = P.GetImage(lvl-1);
+    Image<uint8_t> Idst = P.GetImage(lvl);
+    PyrDownMask(Isrc, Idst);
+  }
+}
 
 }
--- src/preproc/mask.cu
+++ src/preproc/mask.cu
@@ -35,4 +35,48 @@ template void ApplyMask(const Image<uint8_t>& mask, Image<float>& I);
 template void ApplyMask(const Image<uint8_t>& mask, Image<Vector2fda>& I);
 template void ApplyMask(const Image<uint8_t>& mask, Image<Vector3fda>& I);
 
+__global__
+void KernelPyrDownMask(
+    const Image<uint8_t> Iin,
+    Image<uint8_t> Iout
+    ) {
+  const int idx = threadIdx.x + blockDim.x * blockIdx.x;
+  const int idy = threadIdx.y + blockDim.y * blockIdx.y;
+  if (idx < Iout.w_ && idy < Iout.h_) {
+    uint8_t* in0 = Iin.RowPtr(idy*2);
+    uint8_t* in1 = Iin.RowPtr(idy*2+1);
+    uint8_t val00 = in0[idx*2];
+    uint8_t val01 = in0[idx*2+1];
+    uint8_t val10 = in1[idx*2];
+    uint8_t val11 = in1[idx*2+1];
+    uint8_t num = 0;
+    if (val00) {
+      num ++;
+    }
+    if (val01) {
+      num ++;
+    }
+    if (val10) {
+      num ++;
+    }
+    if (val11) {
+      num ++;
+    }
+    Iout(idx,idy) = num;
+  }
+}
+
+void PyrDownMask(
+    const Image<uint8_t>& Iin,
+    Image<uint8_t>& Iout
+    ) {
+  //printf("%dx%d %dx%d\n",Iin.w_,Iin.h_,Iout.w_,Iout.h_);
+  assert(Iin.w_ == Iout.w_*2);
+  assert(Iin.h_ == Iout.h_*2);
+  dim3 threads, blocks;
+  ComputeKernelParamsForImage(blocks,threads,Iout,32,32);
+  KernelPyrDownMask<<<blocks,threads>>>(Iin,Iout);
+  checkCudaErrors(cudaDeviceSynchronize());
+}
+
 }
