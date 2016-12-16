1163fc77029745426845b8a964326b5dd81c3817
Julian Straub <jstraub@csail.mit.edu>
2016-12-16 15:02:31 -0500

    render vbo with value

--- include/tdp/gl/shaders.h
+++ include/tdp/gl/shaders.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <pangolin/gl/glsl.h>
+#include <pangolin/gl/gl.h>
 
 namespace tdp {
 
@@ -19,5 +20,38 @@ class Shaders {
   Shaders& operator=(const Shaders&) = delete;
   static Shaders* shaders_;
 };
+
+void RenderVboValuebo(
+    const pangolin::GlBuffer& vbo,
+    const pangolin::GlBuffer& valuebo,
+    float minVal, float maxVal,
+    const pangolin::OpenGlMatrix& P,
+    const pangolin::OpenGlMatrix& MV
+    ) {
+  pangolin::GlSlProgram& shader = Shaders::Instance()->valueShader_;
+
+  shader.Bind();
+  shader.SetUniform("P", P);
+  shader.SetUniform("MV",MV);
+  shader.SetUniform("minValue", minVal);
+  shader.SetUniform("maxValue", maxVal);
+
+  vbo.Bind();
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); 
+  valuebo.Bind();
+  glVertexAttribPointer(1, 1, GL_FLOAT, GL_FALSE, 0, 0); 
+
+  glEnableVertexAttribArray(0);                                               
+  glEnableVertexAttribArray(1);                                               
+
+  glDrawArrays(GL_POINTS, 0, vbo.num_elements);
+
+  shader.Unbind();
+  glDisableVertexAttribArray(1);
+  valuebo.Unbind();
+  glDisableVertexAttribArray(0);
+  vbo.Unbind();
+
+}
 
 }
