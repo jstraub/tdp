41d93d78ac638f279a4ce60500f00ec62d10313d
Julian Straub <jstraub@csail.mit.edu>
2017-02-26 20:34:18 -0500

    colormap hot directly to opengl

--- include/tdp/utils/colorMap.h
+++ include/tdp/utils/colorMap.h
@@ -12,4 +12,10 @@ Vector3bda ColorMapHot(float cVal) {
           cVal<0.80 ? 0 : 255*(cVal-0.8)*5 );
 }
 
+void glColorHot(float cVal) {
+  glColor3f(cVal<0.20 ? 255*cVal*5 : 255,
+            cVal<0.40 ? 0 : cVal < 0.80 ? 255*(cVal-.4)*2.5 : 255,
+            cVal<0.80 ? 0 : 255*(cVal-0.8)*5 );
+}
+
 }
