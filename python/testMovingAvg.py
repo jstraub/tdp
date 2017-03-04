e60a25dc01d4d9c70abcd0319585f79a31fd9694
Julian Straub <jstraub@csail.mit.edu>
2017-03-03 22:08:13 -0500

    testing moving average; seems to be fine

--- python/testMovingAvg.py
+++ python/testMovingAvg.py
@@ -1,17 +1,30 @@
 import numpy as np
 
-xSum = 0
+xSum = np.zeros(3)
+xSqSum = np.zeros((3,3))
 N = 0
-xSums = []
-for i in range(10000):
-  x = np.random.randn(1)*10+4
-  xSum = (xSum*N/(N+1) + x)
+xVar = []
+xVar2 = []
+xMean = []
+
+mean = 0
+M2  = 0
+for i in range(100000):
+  x = np.random.randn(3)*100+4
+  xSum = (xSum*N + x)/(N+1)
+  xSqSum = (xSqSum*N + np.outer(x,x))/(N+1)
   N = min(500, N+1)
-  xSums.append(xSum/N)
+  xMean.append(xSum)
+  xVar.append(xSqSum - np.outer(xSum,xSum))
+
+  if i > 480 and i < 1020 :
+    print i
+    print xVar[-1]
 
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.plot(xSums)
+plt.plot(xMean)
+plt.plot([m+np.sqrt(xVar[i]) for i,m in enumerate(xMean)])
 plt.show()
 
